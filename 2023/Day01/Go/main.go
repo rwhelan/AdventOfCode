ac6c1be44d1e813987fc0fd27228fa66fdf97050
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2023-12-01 17:00:50 -0500

    Day 01: Puzzle 02

--- 2023/Day01/Go/main.go
+++ 2023/Day01/Go/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"slices"
 )
 
-func FirstDigit(b []byte) int {
-	for _, c := range b {
+var Numbers = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func FirstDigit(b []byte) (int, int) {
+	for i, c := range b {
 		if c >= 48 && c <= 57 {
-			return int(c) - 48
+			return int(c) - 48, i
 		}
 	}
 
 	panic("No digit found")
 }
 
-func LastDigit(input []byte) int {
+func LastDigit(input []byte) (int, int) {
 	b := make([]byte, len(input))
 	copy(b, input)
 
@@ -23,10 +36,71 @@ func LastDigit(input []byte) int {
 	return FirstDigit(b)
 }
 
+func FirstNumber(b []byte) (int, int) {
+	i := 255
+	n := -1
+
+	for k, v := range Numbers {
+		idx := bytes.Index(b, []byte(k))
+		if idx < i && idx != -1 {
+			i = idx
+			n = v
+		}
+	}
+
+	return n, i
+}
+
+func LastNumber(input []byte) (int, int) {
+	i := 255
+	n := -1
+
+	b := make([]byte, len(input))
+	copy(b, input)
+	slices.Reverse(b)
+
+	for s, v := range Numbers {
+		k := []byte(s)
+		slices.Reverse(k)
+		idx := bytes.Index(b, k)
+		if idx < i && idx != -1 {
+			i = idx
+			n = v
+		}
+	}
+
+	return n, i
+}
+
 func PuzzleOne() int {
 	total := 0
 	for _, row := range ReadInputRows() {
-		total += (FirstDigit(row) * 10) + LastDigit(row)
+		f, _ := FirstDigit(row)
+		l, _ := LastDigit(row)
+		total += (f * 10) + l
+	}
+
+	return total
+}
+
+func PuzzleTwo() int {
+	total := 0
+	for _, row := range ReadInputRows() {
+		fd, fdI := FirstDigit(row)
+		ld, ldI := LastDigit(row)
+		fn, fnI := FirstNumber(row)
+		ln, lnI := LastNumber(row)
+
+		f, l := fd, ld
+		if fnI < fdI {
+			f = fn
+		}
+
+		if lnI < ldI {
+			l = ln
+		}
+
+		total += (f * 10) + l
 	}
 
 	return total
@@ -34,4 +108,5 @@ func PuzzleOne() int {
 
 func main() {
 	fmt.Printf("Puzzle One: %d\n", PuzzleOne())
+	fmt.Printf("Puzzle Two: %d\n", PuzzleTwo())
 }
