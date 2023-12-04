57ff04e060aa7f5b350bfb7c81ee81b1c554d9b0
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2023-12-03 23:43:19 -0500

    Day03: Puzzle 01

---
+++ 2023/Day03/Go/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+func IsDigit(b byte) bool {
+	if b >= 48 && b <= 57 {
+		return true
+	}
+
+	return false
+}
+
+func IsSymbol(b byte) bool {
+	return b != '.' && !IsDigit(b)
+}
+
+func SymbolIdx(schematic []byte) map[int]map[int]struct{} {
+	resp := make(map[int]map[int]struct{})
+	for y, row := range bytes.Split(schematic, []byte{'\n'}) {
+		resp[y] = make(map[int]struct{})
+		for x, char := range row {
+			if IsSymbol(char) {
+				resp[y][x] = struct{}{}
+			}
+		}
+	}
+
+	return resp
+}
+
+func Adjacent(x, y int, symbols map[int]map[int]struct{}) bool {
+	for ly := -1; ly < 2; ly++ {
+		for lx := -1; lx < 2; lx++ {
+			if _, ok := symbols[ly+y][lx+x]; ok {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	schematic := ReadInputFile()
+	symbols := SymbolIdx(schematic)
+
+	total := 0
+	for y, row := range bytes.Split(schematic, []byte{'\n'}) {
+		buff := make([]byte, 0, 3)
+		startidx, endidx := 0, 0
+
+		for x, c := range row {
+			if IsDigit(c) {
+				if startidx == 0 {
+					startidx = x
+				}
+
+				endidx = x
+				buff = append(buff, c)
+
+			} else if len(buff) != 0 {
+				if Adjacent(startidx, y, symbols) || Adjacent(endidx, y, symbols) {
+					num, err := strconv.Atoi(string(buff))
+					if err != nil {
+						panic(err)
+					}
+
+					total += num
+				}
+				startidx = 0
+				buff = buff[:0]
+			}
+		}
+
+		if len(buff) != 0 && Adjacent(startidx, y, symbols) || Adjacent(len(row)-1, y, symbols) {
+			num, err := strconv.Atoi(string(buff))
+			if err != nil {
+				panic(err)
+			}
+
+			total += num
+		}
+	}
+
+	fmt.Println(total)
+}
