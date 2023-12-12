3dbff8ce4eebeefbbfc662edc2d1c2bdaa91abec
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2023-12-12 14:40:12 -0500

    Day 11: Puzzle 01 and 02

--- 2023/Day11/Go/main.go
+++ 2023/Day11/Go/main.go
@@ -2,56 +2,48 @@ package main
 
 import "fmt"
 
-func Expand(M Map2D) Map2D {
-	resp := make(Map2D)
-	cols := make(map[int]bool)
+type coord []int
+type coordlist []coord
 
-	offSet := 0
-	for y := 0; y < len(M); y++ {
-		row := M[y]
-		resp[y+offSet] = make(map[int]byte)
-		dupRow := true
-		for x := 0; x < len(row); x++ {
-			if _, ok := cols[x]; !ok {
-				cols[x] = true
-			}
-
-			b := M[y][x]
-			resp[y+offSet][x] = b
-			if b != '.' {
-				cols[x] = false
-				dupRow = false
-			}
-		}
-
-		if dupRow {
-			offSet++
-			fmt.Println(y, y+offSet)
-			resp[y+offSet] = make(map[int]byte)
-			for i := 0; i < len(row); i++ {
-				resp[y+offSet][i] = '.'
-			}
-		}
+func Abs(i int) int {
+	if i < 0 {
+		return -i
 	}
 
-	return resp
+	return i
 }
 
-func PrintMap(M Map2D) {
-	for y := 0; y < len(M); y++ {
-		row := M[y]
-		for x := 0; x < len(row); x++ {
-			fmt.Print(string(M[y][x]))
+func calcDist(l, r coord) int {
+	return Abs(l[0]-r[0]) + Abs(l[1]-r[1])
+}
+
+func AddDists(M Map2D) int {
+	clist := make(coordlist, 0)
+
+	for y, row := range M {
+		for x, b := range row {
+			if b == '#' {
+				clist = append(clist, coord{y, x})
+			}
 		}
-		fmt.Println()
 	}
+
+	distance := 0
+	for i := 0; i < len(clist); i++ {
+		for j := i + 1; j < len(clist); j++ {
+			distance += calcDist(clist[i], clist[j])
+		}
+	}
+
+	return distance
 }
 
 func main() {
 	M := ReadInputMap()
-	eM := Expand(M)
 
-	PrintMap(M)
-	fmt.Println()
-	PrintMap(eM)
+	Mp1 := Expand(M, 2)
+	Mp2 := Expand(M, 1000000)
+
+	fmt.Println("Puzzle One:", AddDists(Mp1))
+	fmt.Println("Puzzle Two:", AddDists(Mp2))
 }
