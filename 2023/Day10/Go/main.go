6a1a9f7634b6dc7db43d19729026d6415ad0acdd
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2023-12-11 22:10:47 -0500

    Day 10: Puzzle 02: Almost Complete; got lucky

--- 2023/Day10/Go/main.go
+++ 2023/Day10/Go/main.go
@@ -5,60 +5,70 @@ import "fmt"
 type Previous int
 
 const (
-	ABOVE Previous = iota
+	_NONE Previous = iota
+	ABOVE
 	RIGHT
 	BELOW
 	LEFT
 )
+
+type Path struct {
+	P map[int]map[int]struct{}
+}
+
+func NewPath() Path {
+	return Path{
+		P: make(map[int]map[int]struct{}),
+	}
+}
+
+func (p Path) Add(Y, X int) {
+	if _, ok := p.P[Y]; !ok {
+		p.P[Y] = make(map[int]struct{})
+	}
+
+	p.P[Y][X] = struct{}{}
+}
+
+func (p Path) IsPath(Y, X int) bool {
+	_, ok := p.P[Y][X]
+	return ok
+}
 
 type Cursor struct {
 	Y, X    int
 	Current byte
 	M       Map2D
 	Prev    Previous
+	Path    Path
 }
 
-func (c *Cursor) ReadAbove() (byte, bool) {
-	b, ok := c.M[c.Y-1][c.X]
-	return b, ok
-}
-
-func (c *Cursor) ReadBelow() (byte, bool) {
-	b, ok := c.M[c.Y+1][c.X]
-	return b, ok
-}
-
-func (c *Cursor) ReadRight() (byte, bool) {
-	b, ok := c.M[c.Y][c.X+1]
-	return b, ok
-}
-
-func (c *Cursor) ReadLeft() (byte, bool) {
-	b, ok := c.M[c.Y][c.X-1]
-	return b, ok
+func (c *Cursor) update() {
+	c.Current = c.M[c.Y][c.X]
+	c.Path.Add(c.Y, c.X)
 }
 
 func (c *Cursor) MoveUp() {
 	c.Y = c.Y - 1
-	c.Current = c.M[c.Y][c.X]
+	c.update()
 	c.Prev = BELOW
 }
 
 func (c *Cursor) MoveDown() {
 	c.Y = c.Y + 1
-	c.Current = c.M[c.Y][c.X]
+	c.update()
 	c.Prev = ABOVE
 }
 
 func (c *Cursor) MoveRight() {
 	c.X = c.X + 1
-	c.Current = c.M[c.Y][c.X]
+	c.update()
 	c.Prev = LEFT
 }
 
 func (c *Cursor) MoveLeft() {
 	c.X = c.X - 1
-	c.Current = c.M[c.Y][c.X]
+	c.update()
 	c.Prev = RIGHT
 }
 
@@ -71,6 +81,7 @@ func NewCursor(M Map2D) *Cursor {
 					X:       x,
 					M:       M,
 					Current: M[y][x],
+					Path:    NewPath(),
 				}
 			}
 		}
@@ -79,9 +90,87 @@ func NewCursor(M Map2D) *Cursor {
 	panic("Start Not Found")
 }
 
+type Scanner struct {
+	count     int
+	inside    bool
+	online    bool
+	lineEntry Previous
+}
+
+func NewScanner() Scanner {
+	return Scanner{
+		inside: false,
+	}
+}
+
+func (s Scanner) CountInside(M Map2D, path Path) int {
+	for i := 0; i < len(M); i++ {
+		y := i
+		row := M[i]
+		s.inside = false
+		for j := 0; j < len(row); j++ {
+			x := j
+			b := row[j]
+			found := false
+			switch {
+			case !path.IsPath(y, x) && s.inside:
+				found = true
+				s.count++
+
+			case path.IsPath(y, x) && b == '|':
+				s.inside = !s.inside
+
+			case path.IsPath(y, x) && b == 'F':
+				s.online = true
+				s.lineEntry = BELOW
+
+			case path.IsPath(y, x) && b == 'L':
+				s.online = true
+				s.lineEntry = ABOVE
+
+			case path.IsPath(y, x) && s.online == true && s.lineEntry == BELOW && b == 'J':
+				s.online = false
+				s.inside = !s.inside
+
+			case path.IsPath(y, x) && s.online == true && s.lineEntry == ABOVE && b == 'J':
+				s.online = false
+
+			case path.IsPath(y, x) && s.online == true && s.lineEntry == ABOVE && b == '7':
+				s.online = false
+				s.inside = !s.inside
+
+			case path.IsPath(y, x) && s.online == true && s.lineEntry == BELOW && b == '7':
+				s.online = false
+
+			}
+
+			if s.inside {
+				fmt.Print("\033[31;1m")
+			} else {
+				fmt.Print("\033[0m")
+			}
+			switch {
+			case path.IsPath(y, x):
+				fmt.Print(string(b))
+
+			case found:
+				fmt.Printf("\033[32;1m%s", string(b))
+
+			default:
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+
+	return s.count
+}
+
 func main() {
-	C := NewCursor(ReadInputMap())
+	inputMap := ReadInputMap()
+	C := NewCursor(inputMap)
 	C.MoveUp()
+	// C.MoveDown()
 
 	count := 1
 
@@ -179,5 +268,8 @@ walk:
 		count++
 	}
 
-	fmt.Println(count)
+	fmt.Println("Puzzle One: ", count/2)
+
+	scanner := NewScanner()
+	fmt.Println("Puzzle Two: ", scanner.CountInside(inputMap, C.Path))
 }
